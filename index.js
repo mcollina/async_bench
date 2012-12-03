ea6a8967e2fdd0541afd78c497488968a1c39b45
Matteo Collina <matteo.collina@gmail.com>
2012-12-03 22:35:25 +0100

    Added a buildPipe function to build the bench pipe.

--- index.js
+++ index.js
@@ -5,22 +5,11 @@ var counters = require("./lib/counters");
 var Timer = require("timer");
 
 module.exports = function runner(opts) {
-  var setup = opts.setup || funcUtils.empty;
-  var bench = opts.bench || funcUtils.empty;
-  var teardown = opts.teardown || funcUtils.empty;
   var complete = opts.complete || funcUtils.empty;
-
   var timer = Timer();
 
   counters.preHeatRunCounter(opts, function(callback) {
-
-    async.waterfall([
-                    setup,
-                    timer.start,
-                    bench,
-                    timer.stop,
-                    teardown
-    ], callback);
+    async.waterfall(funcUtils.buildPipe(timer), callback);
   }, function(err) {
     if(err)
       throw err;
--- test/func_utils_spec.js
+++ test/func_utils_spec.js
@@ -48,4 +48,55 @@ describe(funcUtils, function () {
       expect(spy).to.have.been.calledWith("first", "second");
     });
   });
+
+  describe(funcUtils.buildPipe, function() {
+    var timer;
+    var opts;
+
+    beforeEach(function() {
+      timer = {};
+      opts = {};
+    });
+
+    it("should return an array of five elements", function() {
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("length", 5);
+    });
+
+    it("should put the setup function at the first place", function() {
+      opts.setup = function() {};
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("0", opts.setup);
+    });
+
+    it("should put the empty function if the setup function is null", function() {
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("0", funcUtils.empty);
+    });
+
+    it("should put the bench function at the third place", function() {
+      opts.bench = function() {};
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("2", opts.bench);
+    });
+
+    it("should put the empty function if the bench function is null", function() {
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("2", funcUtils.empty);
+    });
+
+    it("should put the teardown function at the fifth place", function() {
+      opts.teardown = function() {};
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("4", opts.teardown);
+    });
+
+    it("should put the empty function if the teardown function is null", function() {
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("4", funcUtils.empty);
+    });
+
+    it("should put the start function at the first place", function() {
+      timer.start = function() {};
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("1", timer.start);
+    });
+
+    it("should put the stop function at the first place", function() {
+      timer.stop = function() {};
+      expect(funcUtils.buildPipe(opts, timer)).to.have.property("3", timer.stop);
+    });
+  });
 });
